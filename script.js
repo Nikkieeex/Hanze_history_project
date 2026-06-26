84797838829a40cc66937f5e940ef906d1fd2df3
MystikaHanze <sheila.mulder.hanze@gmail.com>
2026-06-26 17:26:23 +0200

    Added images for the Prins Claus Conservatorium, W-A-S and added a 'Did you know' card about Kasteel Zernike. Also updated the sources to chicago author-date format for the Van Doorenveste and the Van OlstBorg in the interactive map.

--- script.js
+++ script.js
@@ -526,10 +526,10 @@ const locations = [
     coords: [53.2409950927156, 6.533645891170895],
     paragraph_one: "Het Van Doorenveste gebouw op het Zernike Campus (Zernikeplein 11) is vernoemd naar oud directeur MTS (Middelbare Technische School) en lid van het verzet Pieter Hermanus Everhardus van Dooren (1899-1944).",
     image_one: "./images/b0a41e61c84bfc711971b7ba3a1805a6vandooren.jpeg",
-    sourceimage_one: "“Pieter Hermanus Everhardus van Dooren.” 2022. Oorlogsgravenstichting. 2022. https://oorlogsgravenstichting.nl/personen/35112/pieter-hermanus-everhardus-van-dooren.",
+    sourceimage_one: "Oorlogsgravenstichting. 2024. “Pieter Hermanus Everhardus Van Dooren.” October 12, 2024. https://oorlogsgravenstichting.nl/personen/35112/pieter-hermanus-everhardus-van-dooren.",
     paragraph_two: "P.H.E. van Dooren is directeur geweest van Academie Minerva (1925-1943) (Academie Minerva (2), 1829 - 1978) en de MTS (Middelbare Technische School).",
     image_two: "./images/1986_04671_mtsacademieminerva.jpg",
-    sourceimage_two: "“M.T.S. ‘Academie Minerva’ En Zeevaartschool, Groningen, Luchtfoto.” 2026. Beeldbank Groningen. 2026. https://www.beeldbankgroningen.nl/beelden/detail/b9242965-1fa0-8454-e19e-cc70be2934ec/media/5eceba86-79d6-90d5-2a75-a01c3e32e491?mode=detail&view=horizontal&q=zeevaartschool%20groningen&rows=1&page=4",
+    sourceimage_two: "“M.T.S. ‘Academie Minerva’ En Zeevaartschool, Groningen, Luchtfoto.” n.d. Beeldbank Groningen. https://www.beeldbankgroningen.nl/beelden/detail/b9242965-1fa0-8454-e19e-cc70be2934ec/media/5eceba86-79d6-90d5-2a75-a01c3e32e491?mode=detail&view=horizontal&q=zeevaartschool%20groningen&rows=1&page=4.",
     paragraph_three: "Het schoolgebouw van de scheepvaartschool en MTS (Middelbare Technische School), wat lag aan de Petrus Driessenstraat 3, speelde een belangrijke rol in de tweede wereldoorlog. Dit schoolgebouw ving leerlingen van andere scholen op en werd gebruikt door het verzet. Onderduikers werden opgevangen in de kelders van de school en door docenten en studenten werden er illegale kranten verspreid en persoonsbewijzen geregeld. Beide directieleden van de zeevaartschool en het MTS waren ook verzetsstrijders; Waaronder de directeur P.H.E. van Dooren. P.H.E. van Dooren heeft geleefd van 1899 tot 1944 en is doodgeschoten door 'foute' politieman Stienus Bertus van Wijnen tijdens de tweede wereldoorlog (“Dooren, Van, Pieter Hermannus Everhardus - TracesOfWar.nl” 2026). Later in 2004 toen de school inmiddels 13 lectoraten had gekregen volgde er de mogelijkvoor om samen een school binnen de Hanze op te richten. Hierbij kwam ook het Instituur of Life Science & Technology (ILST) op. In 2008 werd het gebouw van de faculteit Techniek vernoemd naar P.H.E. van Dooren (1899-1944), de oud-directeur van de scheepvaartschool en het MTS (Middelbare Technische School) en verzetslid tijdens de tweede wereldoorlog (1940-1945)."
   },
   {
@@ -537,7 +537,7 @@ const locations = [
     coords: [53.24030990487017, 6.531974591799187],
     paragraph_one: "De Van OlstBorg (Van OlstToren) (HanzeHogeschool Groningen 2026) op het Zernike Campus (Zernikeplein 7) is vernoemd naar Gerrit van Olst (1734-1807).",
     image_one: "./images/Hanze_University_Groningen_Towerolstborg.jpg",
-    sourceimage_one: "“File:Hanze University Groningen Tower.jpg - Wikimedia Commons.” 2022. Wikimedia.org. 2022. https://commons.wikimedia.org/wiki/File:Hanze_University_Groningen_Tower.jpg ",
+    sourceimage_one: "File:Hanze University Groningen Tower.jpg - Wikimedia Commons. n.d. https://commons.wikimedia.org/wiki/File:Hanze_University_Groningen_Tower.jpg.",
     paragraph_two: "Gerrit van Olst was één van de oprichters van de eerste rechtsvoorganger van de huidige HanzeHogeschool. Van Olst was lid van de Groninger department van het nut (“Marjan Brouwers: Van Meester Tot Master | Groninger Boeken” 2018) en maakte zich sterk voor de oprichting van de Akademie van Teeken-, Bouw- en Zeevaartkunde (9 November, 1797) (Concept7, Groningen Nederland 2026). Op 15 October 1798 volgde de openingsles van de afdeling Bouw- en Zeevaartkunde (“Gevonden in Delpher - Nieuwsblad van Het Noorden” 2026). Vanwege het feit dat veel mensen bereid waren om jaarlijks ‘ene somma van vijf gulden en vijf stuivers’ bij te dragen werd de academie voor ‘schone en nuttige kunsten’ op 1 februari in 1798 een feit. Later (vanaf 1831) werd de onderwijsinstelling ‘Academie Minerva’ genoemd, doordat de kweekschool dertig jaar later van ’t Nut over is gegaan naar het Rijk kreeg de instelling een andere naam. In 1986 zijn beide instellingen opgegaan in de nieuw gevormde Rijkshogeschool Groningen. En door een fusie met de, eveneens in de jaren tachtig ontstane, Hanzehogeschool, varen de twee sinds 1993 onder die vlag verder (Concept7, Groningen Nederland 2026).",
     image_two: "",
     sourceimage_two: "",
@@ -547,34 +547,34 @@ const locations = [
     name: "Marie KamphuisBorg (ZP23)",
     coords: [53.24211126245972, 6.536600661106534],
     paragraph_one: "De Marie KamphuisBorg op het Zernike Campus (Zernikeplein 23) is vernoemd naar Marie Kamphuis (1907-2004), één van de grondleggers van het maatschappelijk werk in Nederland. Het gebouw huisvest de studie Social Work.",
-    image_one: "",
+    image_one: "./images/e21a9a2a-591c-e7e8-ae6e-00f837fb4bf1mariekamphuis.jpg",
     sourceimage_one: "Buursema, C. (2023, June 19). De namen van de Hanze: waar komen ze vandaan en waarom? HanzeMag. https://hanzemag.nl/de-namen-van-de-hanze-waar-komen-ze-vandaan-en-waarom/",
     paragraph_two: "In 1937 ging Kamphuis aan het werk bij het Centraal Instituut voor Christelijke Sociale Arbeid (CICSA) in Amsterdam. Tijdens de Tweede Wereldoorlog moest ze haar werk in het geheim doen: zonder dat de overheid het wist, zette ze in 1943 een CICSA-vestiging op in Groningen, waarvan ze zelf directeur werd. In 1948 kreeg deze Groningse school de naam Academie voor Sociale en Culturele Arbeid (ASCA).",
-    image_two: "",
+    image_two: "./images/2485_3017Universiteits-complex_Zernike-met-latere_Marie_KamphuisBorg.jpg",
     sourceimage_two: "Wikipedia-bijdragers. (2026, January 1). Marie Kamphuis. Wikipedia. https://nl.wikipedia.org/wiki/Marie_Kamphuis",
-    paragraph_three: "Dit is nog een korte samenvatting op basis van de aangeleverde brondocumenten; foto's en exacte bronvermelding kunnen later worden aangevuld."
+    paragraph_three: ""
   },
   {
     name: "Willem-Alexander Sportcentrum (ZP17)",
     coords: [53.24146481154212, 6.53553203412747],
     paragraph_one: "Het Willem-Alexander Sportcentrum (WAS) op het Zernike Campus (Zernikeplein 17) werd in 2010 ontworpen door AGS Architekten en Planners en is vernoemd naar (destijds) Prins Willem-Alexander.",
-    image_one: "",
-    sourceimage_one: "File:20120816 Willem Alexander Sportcentrum Groningen NL (1).jpg - Wikimedia Commons. (2012, August 16). https://commons.wikimedia.org/wiki/File:20120816_Willem_Alexander_Sportcentrum_Groningen_NL_(1).jpg",
-    paragraph_two: "Het sportcentrum wordt gebruikt door studenten en medewerkers van de Hanze en de Rijksuniversiteit Groningen voor sport en bewegen op de campus.",
-    image_two: "",
-    sourceimage_two: "",
-    paragraph_three: "Dit is nog een korte samenvatting op basis van de aangeleverde brondocumenten; verdere geschiedenis en foto's kunnen later worden aangevuld."
+    image_one: "./images/King_Willem-Alexander_2025.jpg",
+    sourceimage_one: "",
+    paragraph_two: "Het sportcentrum wordt gebruikt door studenten en medewerkers van de Hanze en de Rijksuniversiteit Groningen voor sport en beweging op de campus.",
+    image_two: "./images/20120816_Willem_Alexander_Sportcentrum_Groningen_NL_(1).jpg",
+    sourceimage_two: "File:20120816 Willem Alexander Sportcentrum Groningen NL (1).jpg - Wikimedia Commons. (2012, August 16). https://commons.wikimedia.org/wiki/File:20120816_Willem_Alexander_Sportcentrum_Groningen_NL_(1).jpg",
+    paragraph_three: ""
   },
   {
     name: "BrugsmaBorg (ZP9)",
     coords: [53.2400576467666, 6.532911822789884],
     paragraph_one: "De BrugsmaBorg op het Zernike Campus (Zernikeplein 9), waar de PABO is gehuisvest, is vernoemd naar pedagoog Berend Brugsma (1797-1868).",
-    image_one: "",
+    image_one: "./images/berendbrugsma.jpg",
     sourceimage_one: "Buursema, C. (2023, June 19). De namen van de Hanze: waar komen ze vandaan en waarom? HanzeMag. https://hanzemag.nl/de-namen-van-de-hanze-waar-komen-ze-vandaan-en-waarom/",
     paragraph_two: "Brugsma was een arme, in Groningen geboren wees die onder de hoede kwam van hoogleraar Theodorus van Swinderen. Hij werd op 17-jarige leeftijd hoofd van de school in Euvelgunne en in 1861 de eerste directeur van de Rijkskweekschool te Groningen. Hij staat bekend als één van de onderwijsvernieuwers van zijn tijd en wordt ook wel 'de Nederlandse Pestalozzi' genoemd.",
     image_two: "",
     sourceimage_two: "Wikipedia-bijdragers. (2024, December 3). Berend Brugsma. Wikipedia. https://nl.wikipedia.org/wiki/Berend_Brugsma",
-    paragraph_three: "Dit is nog een korte samenvatting op basis van de aangeleverde brondocumenten; foto's en verdere details kunnen later worden aangevuld."
+    paragraph_three: ""
   },
   {
     name: "Academie Minerva (Praediniussingel 59)",
@@ -585,7 +585,7 @@ const locations = [
     paragraph_two: "De MTS (later HTS) en de Hogere Zeevaartschool (HZS) groeiden na de Tweede Wereldoorlog, waardoor er steeds minder ruimte overbleef voor de kunstafdeling van Academie Minerva.",
     image_two: "",
     sourceimage_two: "",
-    paragraph_three: "Dit is nog een korte samenvatting op basis van de aangeleverde brondocumenten; verdere details en foto's kunnen later worden aangevuld."
+    paragraph_three: ""
   },
   {
     name: "Academie Minerva (Gedempte Zuiderdiep 158)",
@@ -607,18 +607,18 @@ const locations = [
     paragraph_two: "In 1922 werd Wiebenga benoemd tot directeur van de School voor Nijverheidsonderwijs in Groningen. Eén van zijn eerste taken was het ontwerpen van een nieuw schoolgebouw: in zes weken had hij het ontwerp klaar, en iets meer dan een jaar later was de bouw afgerond.",
     image_two: "",
     sourceimage_two: "Groninger Archieven. (n.d.). Academie Minerva (2), 1829 - 1978. groningerarchieven.nl.",
-    paragraph_three: "Dit is nog een korte samenvatting op basis van de aangeleverde brondocumenten; verdere details en foto's kunnen later worden aangevuld."
+    paragraph_three: ""
   },
   {
     name: "Prins Claus Conservatorium (Meeuwerderweg 1)",
     coords: [53.21443004896757, 6.577585919906768],
     paragraph_one: "Het Prins Claus Conservatorium aan de Meeuwerderweg is één van de negen conservatoria in Nederland en maakt deel uit van de Hanze als plek voor muziekvakonderwijs.",
-    image_one: "",
+    image_one: "./images/krantconservatorium.png",
     sourceimage_one: "Wikipedia-bijdragers. (2026, May 5). Prins Claus Conservatorium. Wikipedia. https://nl.wikipedia.org/wiki/Prins_Claus_Conservatorium",
     paragraph_two: "In 1965 werd het Stedelijk Muzieklyceum van Groningen omgezet in een conservatorium. In 1990 werd het Conservatorium van Leeuwarden opgeheven en overgeheveld naar Groningen. Sinds 2005 draagt het conservatorium de naam van Prins Claus (1926-2002).",
-    image_two: "",
+    image_two: "./images/prinsclaus.jpg",
     sourceimage_two: "",
-    paragraph_three: "Dit is nog een korte samenvatting op basis van de aangeleverde brondocumenten; verdere details en foto's kunnen later worden aangevuld."
+    paragraph_three: ""
   }
 ];
 
